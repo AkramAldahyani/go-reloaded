39cc35735c65cafd68135e9a6876e5b6402e70f2
unknown <akramta205@icloud.com>
2024-08-06 22:34:06 +0300

    saving the work

--- main/main.go
+++ main/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"car"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	files := os.Args[1:]
+	if len(files) == 2{
+		dataFile := files[0]
+		da, err := os.ReadFile(dataFile)
+		if err != nil {
+			fmt.Println("Please enter a valid file.")
+			return
+		}else{
+			var data []string
+			dat := string(da)
+			data = car.StringToArray(dat)
+			for i := 0; i < len(data); i++ {
+				fmt.Println(data[i])
+			}
+		}
+
+	}else {
+		fmt.Print("Incorrect argument format! Please try again.")
+	}
+	
 }
+
---
+++ stringtoarray.go
@@ -0,0 +1,42 @@
+package car
+
+func StringToArray(s string) []string {
+	var word string
+	var arr []string
+	data := string(s) + " "
+	for i := 0; i < len(data); i++ {
+		if string(data[i]) == "(" {
+			exit := false
+			for !exit && i < len(data) {
+				word += string(data[i])
+				if string(data[i]) == ")" {
+					exit = true
+				}
+				i++
+			}
+			if word != "" {
+				arr = append(arr, word)
+				word = ""
+			}
+
+		} else if string(data[i]) == "," || string(data[i]) == "." || string(data[i]) == "!" || string(data[i]) == "?" || string(data[i]) == ":" {
+			if word != "" {
+				arr = append(arr, word)
+				word = ""
+			}
+			word = string(data[i])
+			arr = append(arr, word)
+			word = ""
+		} else if string(data[i]) != " " {
+			word += string(data[i])
+		} else {
+			if word != "" {
+				arr = append(arr, word)
+				word = ""
+			}
+			word = ""
+		}
+
+	}
+	return arr
+}
